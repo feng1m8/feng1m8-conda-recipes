4ca9ec4ae2b7b6ae11f90934f35e4e7826eea0e5
Minato Feng <feng1m8@outlook.com>
2024-08-07 21:43:19 +0800

    update ucrt

---
+++ mingw-w64-ucrt-x86_64-chealpix/bld.bat
@@ -0,0 +1,10 @@
+mkdir %BUILD_PREFIX%\Library\dev
+
+set MSYSTEM=UCRT64
+set MSYS2_PATH_TYPE=inherit
+set CHERE_INVOKING=1
+
+bash -lce "./build.sh"
+
+rmdir /s /q %BUILD_PREFIX%\Library\ucrt64
+xcopy /s /e /y %SRC_DIR%\ucrt64\ %BUILD_PREFIX%\Library\ucrt64\
