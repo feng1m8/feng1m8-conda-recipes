a986d5568a068aed89ce42478a4654b45dfe5131
Minato Feng <feng1m8@outlook.com>
2024-10-08 11:56:48 +0800

    rename

--- gmake/bld.bat
+++ gmake/bld.bat
@@ -1,1 +1,1 @@
-cl gmake.c gnumake.cxx -I%BUILD_PREFIX%\Library -std:c++17 -EHsc -O1 -Os -Gy -MD -link -FIXED -out:%LIBRARY_BIN%\gmake.exe
+cl symlink.c fname.cxx -I%BUILD_PREFIX%\Library -std:c++17 -EHsc -O1 -Os -Gy -MD -link -FIXED -out:%LIBRARY_BIN%\gmake.exe
--- pkg-config-is-pkgconf/bld.bat
+++ pkg-config-is-pkgconf/bld.bat
@@ -1,1 +1,1 @@
-cl pkg-config.c pkgconf.cxx -I%BUILD_PREFIX%\Library -std:c++17 -EHsc -O1 -Os -Gy -MD -link -FIXED -out:%LIBRARY_BIN%\pkg-config.exe
+cl symlink.c fname.cxx -I%BUILD_PREFIX%\Library -std:c++17 -EHsc -O1 -Os -Gy -MD -link -FIXED -out:%LIBRARY_BIN%\pkg-config.exe
