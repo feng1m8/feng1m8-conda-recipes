a2a6aef37edf4cc8e812d20c6f951cb3cea4b438
Minato Feng <feng1m8@outlook.com>
2024-12-01 12:13:02 +0800

    ar5ivist link libperl

--- ar5ivist/ar5ivist.cxx
+++ ar5ivist/ar5ivist.cxx
@@ -1,59 +1,88 @@
 #include <filesystem>
+#include <windows.h>
 
-#include <shlwapi.h>
-#pragma comment(lib, "shlwapi.lib") 
+#define selectany
+#include <EXTERN.h>
+#include <perl.h>
+#undef selectany
 
-extern "C"
+EXTERN_C void xs_init(pTHX);
+
+class Perl
 {
-    char *quoted(const char *data);
-    int create_and_wait_for_subprocess(char* command);
-    char* join_executable_and_args(char *executable, char **args, int argc);
-}
+    PerlInterpreter *my_perl;
 
-static std::string to_string(const std::wstring &wstr)
+    public:
+    Perl(int argc, char *argv[], char *envp[])
+    {
+        PERL_SYS_INIT3(&argc, &argv, &envp);
+        my_perl = perl_alloc();
+        perl_construct( my_perl );
+        PL_exit_flags |= PERL_EXIT_DESTRUCT_END;
+    }
+
+    ~Perl()
+    {
+        perl_destruct(my_perl);
+        perl_free(my_perl);
+        PERL_SYS_TERM();
+    }
+
+    std::filesystem::path sitebin() const
+    {
+        const char *embedding[] = {"", "-e", "0", NULL};
+        perl_parse(this->my_perl, xs_init, 3, const_cast<char**>(embedding), NULL);
+
+        std::filesystem::path sitelib(SvPV_nolen(eval_pv("use LaTeXML; $INC{\"LaTeXML.pm\"}", TRUE)));
+        return sitelib.parent_path().replace_filename("bin");
+    }
+
+    void run(std::vector<std::string> newargv) const
+    {
+        std::vector<char *> newargs;
+        newargv.reserve(newargv.size() + 1);
+
+        for (auto &i: newargv)
+            newargs.emplace_back(i.data());
+        newargs.emplace_back(nullptr);
+
+        perl_parse(this->my_perl, xs_init, newargs.size() - 1, newargs.data(), NULL);
+        perl_run(this->my_perl);
+    }
+};
+
+int main(int argc, char *argv[], char *envp[])
 {
-    const wchar_t* wptr = wstr.c_str();
-    std::mbstate_t ps;
-    std::string str(std::wcsrtombs(nullptr, &wptr, 0, &ps), '\0');
-    std::wcsrtombs(str.data(), &wptr, str.size(), &ps);
-    return str;
-}
+    char newpath[MAX_PATH];
+    GetModuleFileNameA(NULL, newpath, MAX_PATH);
 
-#pragma comment(linker, "/subsystem:console")
-#pragma comment(linker, "/entry:wmainCRTStartup")
-int wmain(int argc, wchar_t* argv[])
-{
-    std::string newpath("perl.exe");
-    newpath.resize(MAX_PATH, '\0');
+    const std::filesystem::path ar5iv(std::filesystem::path(newpath).parent_path().replace_filename("opt") / "ar5iv-bindings");
 
-    if (FALSE == PathFindOnPathA(newpath.data(), NULL))
-        throw std::filesystem::filesystem_error(newpath, std::make_error_code(std::errc::no_such_file_or_directory));
+    const Perl perl(argc, argv, envp);
 
-    const std::filesystem::path prefix(std::filesystem::path(newpath).parent_path().parent_path());
-
-    std::vector<char *> newargs{
-        newpath.data(),
-        quoted((prefix / "site" / "bin" / "latexmlc").string().c_str()),
-        quoted("--preload=[nobibtex,ids,localrawstyles,nobreakuntex,magnify=1.8,zoomout=1.8,tokenlimit=249999999,iflimit=3599999,absorblimit=1299999,pushbacklimit=599999]latexml.sty"),
-        quoted("--preload=ar5iv.sty"),
-        quoted(("--path=" + (prefix / "opt" / "ar5iv-bindings" / "bindings").string()).c_str()),
-        quoted(("--path=" + (prefix / "opt" / "ar5iv-bindings" / "supported_originals").string()).c_str()),
-        quoted("--format=html5"),
-        quoted("--pmml"),
-        quoted("--cmml"),
-        quoted("--mathtex"),
-        quoted("--timeout=2700"),
-        quoted("--noinvisibletimes"),
-        quoted("--nodefaultresources"),
-        quoted("--css=https://cdn.jsdelivr.net/gh/dginev/ar5iv-css@0.7.9/css/ar5iv.min.css"),
-        quoted("--css=https://cdn.jsdelivr.net/gh/dginev/ar5iv-css@0.7.9/css/ar5iv-fonts.min.css"),
+    std::vector<std::string> newargv{
+        "perl.exe",
+        (perl.sitebin() / "latexmlc").string(),
+        "--preload=[nobibtex,ids,localrawstyles,nobreakuntex,magnify=1.8,zoomout=1.8,tokenlimit=249999999,iflimit=3599999,absorblimit=1299999,pushbacklimit=599999]latexml.sty",
+        "--preload=ar5iv.sty",
+        "--path=" + (ar5iv / "bindings").string(),
+        "--path=" + (ar5iv / "supported_originals").string(),
+        "--format=html5",
+        "--pmml",
+        "--cmml",
+        "--mathtex",
+        "--timeout=2700",
+        "--noinvisibletimes",
+        "--nodefaultresources",
+        "--css=https://cdn.jsdelivr.net/gh/dginev/ar5iv-css@0.7.9/css/ar5iv.min.css",
+        "--css=https://cdn.jsdelivr.net/gh/dginev/ar5iv-css@0.7.9/css/ar5iv-fonts.min.css",
     };
 
-    newargs.reserve(argc + newargs.size());
+    newargv.reserve(argc + newargv.size() - 1);
     for (int i = 1; i < argc; ++i)
-        newargs.emplace_back(quoted(to_string(argv[i]).c_str()));
-    newargs.emplace_back(nullptr);
+        newargv.emplace_back(argv[i]);
 
-    char *cmdline = join_executable_and_args(newpath.data(), newargs.data(), newargs.size() - 1);
-    return create_and_wait_for_subprocess(cmdline);
+    perl.run(newargv);
+
+    return 0;
 }
--- ar5ivist/bld.bat
+++ ar5ivist/bld.bat
@@ -3,4 +3,9 @@ xcopy /s /e /y %SRC_DIR%\deprecated\ %LIBRARY_PREFIX%\opt\ar5iv-bindings\depreca
 xcopy /s /e /y %SRC_DIR%\originals\ %LIBRARY_PREFIX%\opt\ar5iv-bindings\originals\
 xcopy /s /e /y %SRC_DIR%\supported_originals\ %LIBRARY_PREFIX%\opt\ar5iv-bindings\supported_originals\
 
-cl %RECIPE_DIR%\ar5ivist.cxx %LIBRARY_PREFIX%\symlink-exe.c -DRELATIVE_PATH -std:c++17 -EHsc -MD -O1 -Fe"%LIBRARY_BIN%\ar5ivist.exe"
+perl -MExtUtils::Embed -e xsinit -- -o perlxsi.c
+
+clang -c perlxsi.c -DWIN32 -DWIN64 -D__USE_MINGW_ANSI_STDIO -DPERL_TEXTMODE_SCRIPTS -DPERL_IMPLICIT_CONTEXT -DPERL_IMPLICIT_SYS -DUSE_PERLIO -fwrapv -fno-strict-aliasing -mms-bitfields -I%LIBRARY_PREFIX%\lib\CORE
+clang++ -c %RECIPE_DIR%\ar5ivist.cxx -std=c++17 -DWIN32 -DWIN64 -D__USE_MINGW_ANSI_STDIO -DPERL_TEXTMODE_SCRIPTS -DPERL_IMPLICIT_CONTEXT -DPERL_IMPLICIT_SYS -DUSE_PERLIO -fwrapv -fno-strict-aliasing -mms-bitfields -I%LIBRARY_PREFIX%\lib\CORE
+
+clang++ -o %LIBRARY_BIN%\ar5ivist.exe perlxsi.o ar5ivist.o -s -L%LIBRARY_PREFIX%\lib\CORE %LIBRARY_PREFIX%\lib\CORE\libperl532.a
