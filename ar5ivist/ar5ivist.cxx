9cd3d56373ab6a778650df1e99e9285128ab87da
Minato Feng <feng1m8@outlook.com>
2024-12-01 20:14:47 +0800

    Update ar5ivist.cxx

--- ar5ivist/ar5ivist.cxx
+++ ar5ivist/ar5ivist.cxx
@@ -8,41 +8,39 @@
 
 EXTERN_C void xs_init(pTHX);
 
-class Perl
+class MyPerl
 {
     PerlInterpreter *my_perl;
 
 public:
-    Perl(int argc, char *argv[], char *envp[])
+    MyPerl(int argc, char *argv[], char *envp[])
     {
         PERL_SYS_INIT3(&argc, &argv, &envp);
         this->my_perl = perl_alloc();
-        perl_construct( this->my_perl );
+        perl_construct(this->my_perl);
         PL_exit_flags |= PERL_EXIT_DESTRUCT_END;
     }
 
-    ~Perl()
+    ~MyPerl()
     {
         perl_destruct(this->my_perl);
         perl_free(this->my_perl);
         PERL_SYS_TERM();
     }
 
-    std::filesystem::path sitebin() const
+    std::string eval(const char *p) const
     {
         const char *embedding[] = {"", "-e", "0", NULL};
         perl_parse(this->my_perl, xs_init, 3, const_cast<char**>(embedding), NULL);
-
-        std::filesystem::path sitelib(SvPV_nolen(eval_pv("use LaTeXML; $INC{\"LaTeXML.pm\"}", TRUE)));
-        return sitelib.parent_path().replace_filename("bin");
+        return SvPV_nolen(eval_pv(p, TRUE));
     }
 
-    void run(std::vector<std::string> newargv) const
+    void run(std::vector<std::string> &argv) const
     {
         std::vector<char *> newargs;
-        newargv.reserve(newargv.size() + 1);
+        newargs.reserve(argv.size() + 1);
 
-        for (auto &i: newargv)
+        for (auto &i: argv)
             newargs.emplace_back(i.data());
         newargs.emplace_back(nullptr);
 
@@ -56,17 +54,19 @@ int main(int argc, char *argv[], char *envp[])
     char newpath[MAX_PATH];
     GetModuleFileNameA(NULL, newpath, MAX_PATH);
 
-    const std::filesystem::path ar5iv(std::filesystem::path(newpath).parent_path().replace_filename("opt") / "ar5iv-bindings");
+    const std::filesystem::path opt(std::filesystem::path(newpath).parent_path().replace_filename("opt"));
 
-    const Perl perl(argc, argv, envp);
+    const MyPerl perl(argc, argv, envp);
+
+    const std::filesystem::path site(std::filesystem::path(perl.eval("use LaTeXML; $INC{'LaTeXML.pm'}")).parent_path().parent_path());
 
     std::vector<std::string> newargv{
         "perl.exe",
-        (perl.sitebin() / "latexmlc").string(),
+        (site / "bin" / "latexmlc").string(),
         "--preload=[nobibtex,ids,localrawstyles,nobreakuntex,magnify=1.8,zoomout=1.8,tokenlimit=249999999,iflimit=3599999,absorblimit=1299999,pushbacklimit=599999]latexml.sty",
         "--preload=ar5iv.sty",
-        "--path=" + (ar5iv / "bindings").string(),
-        "--path=" + (ar5iv / "supported_originals").string(),
+        "--path=" + (opt / "ar5iv-bindings" / "bindings").string(),
+        "--path=" + (opt / "ar5iv-bindings" / "supported_originals").string(),
         "--format=html5",
         "--pmml",
         "--cmml",
