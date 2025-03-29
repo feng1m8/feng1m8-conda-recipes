16e428f2f019dff8f4c4aded198bf7e0f8b6661c
Yuan Feng <feng1m8@outlook.com>
2025-03-29 18:48:56 +0800

    Update ccfits 2.7

--- ccfits/bld.bat
+++ ccfits/bld.bat
@@ -1,1 +1,12 @@
+sed -i "s|AC_CONFIG_AUX_DIR(config)|AC_CONFIG_MACRO_DIRS([config/m4])|g" configure.ac
+sed -i "/AC_CANONICAL_TARGET/i\AC_CONFIG_AUX_DIR(config)" configure.ac
+sed -i "/^libCCfits_la_LDFLAGS/s\$\ -no-undefined\g" Makefile.am
+sed -i "/^cookbook_LDFLAGS/s\$\ -static-libtool-libs\g" Makefile.am
+
 call %BUILD_PREFIX%\Library\bin\run_autotools_clang_conda_build.bat
+
+for /f "delims=" %%i in ('cygpath -m %PREFIX%') do set "LIBRARY_PREFIX=%%i"
+sed -i "s|%PREFIX%|%LIBRARY_PREFIX%|g" %LIBRARY_PREFIX%\lib\pkgconfig\CCfits.pc
+
+for /f "delims=" %%i in ('cygpath -w %PREFIX%') do set "LIBRARY_PREFIX=%%i"
+copy /y MSconfig.h %LIBRARY_PREFIX%\include\CCfits
--- ccfits/build.sh
+++ ccfits/build.sh
@@ -1,19 +1,7 @@
-sed -i 's|AC_CONFIG_AUX_DIR(config)|AC_CONFIG_MACRO_DIRS([config/m4])|g' configure.ac
-sed -i '/AC_CANONICAL_TARGET/i\AC_CONFIG_AUX_DIR(config)' configure.ac
-
 autoreconf -ivf
-
 ./configure --prefix=$PREFIX --enable-static
 
 [[ "$target_platform" == "win-64" ]] && patch_libtool
 
-sed -i 's/libCCfits_la_LDFLAGS = -R $(R_LIB_PATH) -R $(CXX_LIB_PATH)/libCCfits_la_LDFLAGS = -no-undefined/g' Makefile
-sed -i 's/cookbook_LDFLAGS = -R $(R_LIB_PATH) -R $(CXX_LIB_PATH)/cookbook_LDFLAGS = -static-libtool-libs/g' Makefile
-
 make
-
-sed -i "s|$PREFIX|$(cygpath -w $PREFIX)|g" CCfits.pc
-
 make install
-
-cp MSconfig.h $PREFIX/include/CCfits/
