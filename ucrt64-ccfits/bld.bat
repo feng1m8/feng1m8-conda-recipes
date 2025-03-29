efc11c6c7e3d8de05c3a48693649b129a6056ddb
Yuan Feng <feng1m8@outlook.com>
2025-03-29 20:28:23 +0800

    Update ucrt64-ccfits 2.7

--- ucrt64-ccfits/bld.bat
+++ ucrt64-ccfits/bld.bat
@@ -1,1 +1,8 @@
+sed -i "s|AC_CONFIG_AUX_DIR(config)|AC_CONFIG_MACRO_DIRS([config/m4])|g" configure.ac
+sed -i "/AC_CANONICAL_TARGET/i\AC_CONFIG_AUX_DIR(config)" configure.ac
+sed -i "/^libCCfits_la_LDFLAGS/s\$\ -no-undefined\g" Makefile.am
+
 call %BUILD_PREFIX%\Library\bin\run_autotools_clang_conda_build.bat
+
+for /f "delims=" %%i in ('cygpath -m %PREFIX%') do set "LIBRARY_PREFIX=%%i"
+sed -i "s|%PREFIX%|%LIBRARY_PREFIX%|g" %LIBRARY_PREFIX%\ucrt64\lib\pkgconfig\CCfits.pc
--- ucrt64-ccfits/build.sh
+++ ucrt64-ccfits/build.sh
@@ -10,19 +10,10 @@ export CXXFLAGS=
 export CPPFLAGS=
 export LDFLAGS=
 
-sed -i 's|AC_CONFIG_AUX_DIR(config)|AC_CONFIG_MACRO_DIRS([config/m4])|g' configure.ac
-sed -i '/AC_CANONICAL_TARGET/i\AC_CONFIG_AUX_DIR(config)' configure.ac
-
 autoreconf -ivf
-
-./configure --prefix=$PREFIX/ucrt64 --with-cfitsio=$BUILD_PREFIX/Library
+./configure --prefix=$PREFIX/ucrt64 --with-cfitsio=$PREFIX
 
 [[ "$target_platform" == "win-64" ]] && patch_libtool
 
-sed -i 's/libCCfits_la_LDFLAGS = -R $(R_LIB_PATH) -R $(CXX_LIB_PATH)/libCCfits_la_LDFLAGS = -no-undefined/g' Makefile
-
 make
-
-sed -i "s|$PREFIX|$(cygpath -w $PREFIX)|g" CCfits.pc
-
 make install
