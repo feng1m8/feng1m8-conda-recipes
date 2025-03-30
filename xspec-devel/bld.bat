af71e86fbfcf288f8959a5a015a642f69eb66b83
Yuan Feng <feng1m8@outlook.com>
2025-03-30 14:42:49 +0800

    Update xspec 12.15.0

--- xspec-devel/bld.bat
+++ xspec-devel/bld.bat
@@ -3,4 +3,11 @@ mkdir %PREFIX%\etc\conda\deactivate.d
 copy %RECIPE_DIR%\activate.bat %PREFIX%\etc\conda\activate.d\%PKG_NAME%_activate.bat
 copy %RECIPE_DIR%\deactivate.bat %PREFIX%\etc\conda\deactivate.d\%PKG_NAME%_deactivate.bat
 
+sed -i "/$HEACORE $TCLTK/d" Xspec/BUILD_DIR/hd_config_info
+sed -i "/HEACORE=heacore/d" Xspec/BUILD_DIR/hd_config_info
+sed -i "/TCLTK=tcltk/d" Xspec/BUILD_DIR/hd_config_info
+
 call %BUILD_PREFIX%\Library\bin\run_autotools_clang_conda_build.bat
+
+for /f "delims=" %%i in ('cygpath -w %PREFIX%') do set "LIBRARY_PREFIX=%%i"
+rmdir /s /q %LIBRARY_PREFIX%\x86_64-pc-mingw64\BUILD_DIR
--- xspec-devel/build.sh
+++ xspec-devel/build.sh
@@ -5,23 +5,16 @@ export AS=x86_64-w64-mingw32-as.exe
 export AR=x86_64-w64-mingw32-ar.exe
 export NM=x86_64-w64-mingw32-nm.exe
 export LD=x86_64-w64-mingw32-ld.exe
+export STRIP=x86_64-w64-mingw32-strip.exe
 export CFLAGS=
 export CXXFLAGS=
 export CPPFLAGS=
 export LDFLAGS=
 
-sed -i '/DEPENDENCIES="$HEACORE $TCLTK"/d' Xspec/BUILD_DIR/hd_config_info
-sed -i '/HEACORE=heacore/d' Xspec/BUILD_DIR/hd_config_info
-sed -i '/TCLTK=tcltk/d' Xspec/BUILD_DIR/hd_config_info
-
-sed -i $'/strcat(full_cmd, "\'")/d' Xspec/BUILD_DIR/hd_install.c
-sed -i $'s|\'/\' != \*inpath|(\'/\' != \*inpath) \& (!strstr(inpath, ":"))|g' Xspec/BUILD_DIR/hd_install.c
-
-sed -i 's/= hd_install$/= hd_install -a/' Xspec/BUILD_DIR/Makefile-std
-
 cd Xspec/BUILD_DIR
 
-./configure --prefix=$PREFIX --with-components="" --disable-x --disable-strip
+autoconf -v -f
+./configure --prefix=$PREFIX --with-components="" --disable-x
 
 ./hmake
 ./hmake install
