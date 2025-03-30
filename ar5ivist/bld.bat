a7cfa2b1f1966f675bb134ef7a9d2f5ff0b4ddd3
Yuan Feng <feng1m8@outlook.com>
2025-03-30 15:02:10 +0800

    Update ar5ivist

--- ar5ivist/bld.bat
+++ ar5ivist/bld.bat
@@ -1,10 +1,10 @@
-xcopy /s /e /y %SRC_DIR%\bindings\ %LIBRARY_PREFIX%\opt\ar5iv-bindings\bindings\
-xcopy /s /e /y %SRC_DIR%\deprecated\ %LIBRARY_PREFIX%\opt\ar5iv-bindings\deprecated\
-xcopy /s /e /y %SRC_DIR%\originals\ %LIBRARY_PREFIX%\opt\ar5iv-bindings\originals\
-xcopy /s /e /y %SRC_DIR%\supported_originals\ %LIBRARY_PREFIX%\opt\ar5iv-bindings\supported_originals\
+xcopy /s /e /y %SRC_DIR%\bindings\ %PREFIX%\opt\ar5iv-bindings\bindings\
+xcopy /s /e /y %SRC_DIR%\deprecated\ %PREFIX%\opt\ar5iv-bindings\deprecated\
+xcopy /s /e /y %SRC_DIR%\originals\ %PREFIX%\opt\ar5iv-bindings\originals\
+xcopy /s /e /y %SRC_DIR%\supported_originals\ %PREFIX%\opt\ar5iv-bindings\supported_originals\
 
 perl -MExtUtils::Embed -e xsinit -- -o perlxsi.c
 
 powershell "clang -c perlxsi.c ((perl -MExtUtils::Embed -e ccopts) -split ' ')"
 powershell "clang++ -c %RECIPE_DIR%\ar5ivist.cxx -std=c++23 ((perl -MExtUtils::Embed -e ccopts) -split ' ')"
-powershell "clang++ -o %LIBRARY_BIN%\ar5ivist.exe perlxsi.o ar5ivist.o ((perl -MExtUtils::Embed -e ldopts) -split ' ')"
+powershell "clang++ -o %PREFIX%\Scripts\ar5ivist.exe perlxsi.o ar5ivist.o ((perl -MExtUtils::Embed -e ldopts) -split ' ')"
