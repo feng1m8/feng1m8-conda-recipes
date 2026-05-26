760b6e2141f6f67ef40f1a9551566298b8d69059
Yuan Feng <feng1m8@outlook.com>
2026-05-26 18:22:45 +0800

    ucrt64-ccfits v1 recipe

--- ucrt64-ccfits/build.sh
+++ ucrt64-ccfits/build.sh
@@ -1,19 +1,34 @@
 export CC=x86_64-w64-mingw32-gcc.exe
 export CXX=x86_64-w64-mingw32-g++.exe
-export RANLIB=x86_64-w64-mingw32-ranlib.exe
-export AS=x86_64-w64-mingw32-as.exe
+export ADDR2LINE=x86_64-w64-mingw32-addr2line.exe
 export AR=x86_64-w64-mingw32-ar.exe
-export NM=x86_64-w64-mingw32-nm.exe
+export AS=x86_64-w64-mingw32-as.exe
+export CXXFILT=x86_64-w64-mingw32-c++filt.exe
+export ELFEDIT=x86_64-w64-mingw32-elfedit.exe
+export GPROF=x86_64-w64-mingw32-gprof.exe
 export LD=x86_64-w64-mingw32-ld.exe
-export CFLAGS=
-export CXXFLAGS=
-export CPPFLAGS=
-export LDFLAGS=
+export NM=x86_64-w64-mingw32-nm.exe
+export OBJCOPY=x86_64-w64-mingw32-objcopy.exe
+export OBJDUMP=x86_64-w64-mingw32-objdump.exe
+export RANLIB=x86_64-w64-mingw32-ranlib.exe
+export READELF=x86_64-w64-mingw32-readelf.exe
+export SIZE=x86_64-w64-mingw32-size.exe
+export STRINGS=x86_64-w64-mingw32-strings.exe
+export STRIP=x86_64-w64-mingw32-strip.exe
+export DLLTOOL=x86_64-w64-mingw32-dlltool.exe
+export DLLWRAP=x86_64-w64-mingw32-dllwrap.exe
+export WINDMC=x86_64-w64-mingw32-windmc.exe
+export WINDRES=x86_64-w64-mingw32-windres.exe
+
+export CFLAGS="-I${LIBRARY_INC//\\//}"
+export CXXFLAGS="$CFLAGS"
+export CPPFLAGS="$CFLAGS"
+export LDFLAGS="-L${LIBRARY_LIB//\\//}"
 
 autoreconf -ivf
-./configure --prefix=$PREFIX/ucrt64 --with-cfitsio=$PREFIX
+./configure --prefix=$PREFIX/ucrt64
 
 [[ "$target_platform" == "win-64" ]] && patch_libtool
 
-make
+make libCCfits_la_LDFLAGS="-no-undefined"
 make install
