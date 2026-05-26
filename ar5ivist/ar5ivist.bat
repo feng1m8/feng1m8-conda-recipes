f381f30ca5f5578cdf4762712eb926d7f73257a6
Yuan Feng <feng1m8@outlook.com>
2026-05-26 18:22:18 +0800

    Update .github\workflows\build.yaml

--- ar5ivist/ar5ivist.bat
+++ ar5ivist/ar5ivist.bat
@@ -8,101 +8,99 @@ exit /b
 #>
 
 
-function New-Ar5ivistTempDir {
-    $Base = "C:\ProgramData\ar5ivist\Temp"
-    if (-not (Test-Path $Base)) {
-        New-Item -ItemType Directory -Path $Base | Out-Null
+function New-TempPath {
+    $TempPath = "C:\ProgramData\ar5ivist\Temp"
+
+    if (-not (Test-Path $TempPath)) {
+        New-Item -ItemType "Directory" -Path $TempPath -Force | Out-Null
     }
 
-    $Dir = Join-Path $Base ([System.IO.Path]::GetRandomFileName())
-    New-Item -ItemType Directory -Path $Dir | Out-Null
-    return $Dir
+    $TempPath = Join-Path $TempPath ([System.IO.Path]::GetRandomFileName())
+    New-Item -ItemType "Directory" -Path $TempPath -Force | Out-Null
+
+    return $TempPath
 }
 
 
-function Update-DesArgs {
+function Update-ArgsValue {
     param(
         [Parameter(ValueFromRemainingArguments = $true)]
-        [string[]]$Args,
+        [string[]]$InputArgs,
 
-        [string]$NewValue = "REPLACED"
+        [string]$NewValue
     )
 
-    $updated = @()
-    $oldValues = @()
+    $NewArgs = @()
+    $OutPath = @()
 
-    for ($i = 0; $i -lt $Args.Count; $i++) {
-        $arg = $Args[$i]
+    for ($i = 0; $i -lt $InputArgs.Count; $i++) {
+        $Arg = $InputArgs[$i]
 
-        if ($arg -match '^--(des[^=]*)=(.+)$') {
-            $key = $matches[1]
-            $old = $matches[2]
+        if ($Arg -match '^--(des[^=]*)=(.+)$') {
+            $Key = $matches[1]
+            $RealPath = $matches[2]
 
-            $path = Split-Path $old -Parent
-            $name = Split-Path $old -Leaf
+            $OutPath = Split-Path $RealPath -Parent
+            $FileName = Split-Path $RealPath -Leaf
 
-            $oldValues = [PSCustomObject]@{
-                Path = $path
-                Name = $name
-            }
-
-            $updated += """--$key=$NewValue\$name"""
+            $NewArgs += """--$Key=$NewValue\$FileName"""
             continue
         }
 
-        if ($arg -match '^--(des.+)$') {
-            $key = $matches[1]
-            $old = $Args[$i + 1]
-
-            $path = Split-Path $old -Parent
-            $name = Split-Path $old -Leaf
-
-            $oldValues = [PSCustomObject]@{
-                Path = $path
-                Name = $name
+        if ($Arg -match '^--(des.+)$') {
+            if ($i + 1 -ge $InputArgs.Count) {
+                $NewArgs += """$Arg"""
+                continue
             }
 
-            $updated += $arg
-            $updated += """$NewValue\$name"""
+            $RealPath = $InputArgs[$i + 1]
+
+            $OutPath = Split-Path $RealPath -Parent
+            $FileName = Split-Path $RealPath -Leaf
+
+            $NewArgs += $Arg
+            $NewArgs += """$NewValue\$FileName"""
 
             $i++
             continue
         }
 
-        $updated += """$arg"""
+        $NewArgs += """$Arg"""
     }
 
     return [PSCustomObject]@{
-        UpdatedArgs = $updated
-        OldValues   = $oldValues
+        NewArgs = $NewArgs
+        OutPath = $OutPath
     }
 }
 
 
-
 function main {
-    if ($args.Count -gt 1) {
-        $rest = $args[1..($args.Count-1)]
+    if ($Args.Count -gt 1) {
+        $RemainingArgs = $Args[1..($Args.Count-1)]
     } else {
-        $rest = @()
+        $RemainingArgs = @()
     }
 
-    $dir = New-Ar5ivistTempDir
-    $update = Update-DesArgs -Args $rest -NewValue $dir
+    $TempPath = New-TempPath
+    $RemainingArgs = Update-ArgsValue -InputArgs $RemainingArgs -NewValue $TempPath
 
-    $ScriptDir = Split-Path -Parent $args[0]
-    $UcrtDir   = Join-Path $ScriptDir "..\ucrt64\bin" | Resolve-Path
-    $latexmlDir   = Join-Path $ScriptDir "..\..\bin" | Resolve-Path
-    $latexmlDir2   = Join-Path $ScriptDir "..\..\Scripts" | Resolve-Path
-    $optDir= Join-Path $ScriptDir "..\opt\ar5iv-bindings" | Resolve-Path
-    $env:PATH = "$UcrtDir;$ScriptDir;$latexmlDir2;$env:PATH"
+    $NewArgs = $RemainingArgs.NewArgs
+    $OutPath = $RemainingArgs.OutPath
 
-    & "$ScriptDir\perl.exe" `
-    "$latexmlDir\latexmlc" `
+    $BinDir = Split-Path -Parent $Args[0]
+    $UcrtDir = Join-Path $BinDir "..\ucrt64\bin" -Resolve
+    $SiteDir = Join-Path $BinDir "..\site\bin" -Resolve
+    $ScriptsDir = Join-Path $BinDir "..\..\Scripts" -Resolve
+    $OptDir = Join-Path $BinDir "..\opt\ar5iv-bindings" -Resolve
+    $env:PATH = "$UcrtDir;$BinDir;$ScriptsDir;$env:PATH"
+
+    & "$BinDir\perl.exe" `
+    "$SiteDir\latexmlc" `
     "--preload=[nobibtex,ids,localrawstyles,nobreakuntex,magnify=1.8,zoomout=1.8,tokenlimit=249999999,iflimit=3599999,absorblimit=1299999,pushbacklimit=599999]latexml.sty" `
     "--preload=ar5iv.sty" `
-    "--path=$optDir\bindings" `
-    "--path=$optDir\supported_originals" `
+    "--path=$OptDir\bindings" `
+    "--path=$OptDir\supported_originals" `
     "--format=html5" `
     "--pmml" `
     "--cmml" `
@@ -112,21 +110,17 @@ function main {
     "--nodefaultresources" `
     "--css=https://cdn.jsdelivr.net/gh/dginev/ar5iv-css@0.7.9/css/ar5iv.min.css" `
     "--css=https://cdn.jsdelivr.net/gh/dginev/ar5iv-css@0.7.9/css/ar5iv-fonts.min.css" `
-    $update.UpdatedArgs
+    $NewArgs
 
-    $targetPath = $update.OldValues.Path    
-    if (![string]::IsNullOrWhiteSpace($targetPath)) {
-        if (-not (Test-Path $targetPath)) {
-            # Write-Host "目标路径不存在，正在创建: $targetPath"
-            New-Item -ItemType Directory -Path $targetPath -Force | Out-Null
+    if (-not ([string]::IsNullOrWhiteSpace($OutPath))) {
+        if (-not (Test-Path $OutPath)) {
+            New-Item -ItemType "Directory" -Path $OutPath -Force | Out-Null
         }
     }
 
-    Copy-Item -Path (Join-Path $dir "*") `
-                -Destination $targetPath `
-                -Recurse -Force
+    Move-Item -Path (Join-Path $TempPath "*") -Destination $OutPath -Force
 
-    if (Test-Path $dir) {
-        Remove-Item $dir -Recurse -Force
+    if (Test-Path $TempPath) {
+        Remove-Item $TempPath -Recurse -Force
     }
 }
